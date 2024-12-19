899f4985024d4f2580f1278834e45a5644324521
NguyenTuanNhut <nhutnguyen2k6.py@gmail.com>
2024-12-19 20:35:04 +0700

    Update b6.c

--- b6.c
+++ b6.c
@@ -2,7 +2,6 @@
 int main(){
     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     int sum=0;
-    }
     int j=0;
     for(int i=0;i<sizeof(arr[0])/sizeof(arr[0][0]);i++){
         printf("%d",arr[i][j]);
