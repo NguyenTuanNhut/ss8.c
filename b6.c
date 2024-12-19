ec20cb2b658db0f7d2c8b6e40482a4fe85f3098d
NguyenTuanNhut <nhutnguyen2k6.py@gmail.com>
2024-12-19 20:34:19 +0700

    Update b6.c

--- b6.c
+++ b6.c
@@ -2,14 +2,11 @@
 int main(){
     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     int sum=0;
-    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
-                 for(int j=0;j<sizeof(arr[0])/sizeof(arr[0][0]);j++){
-                       sum+=arr[i][j];
-                 }
     }
     int j=0;
     for(int i=0;i<sizeof(arr[0])/sizeof(arr[0][0]);i++){
         printf("%d",arr[i][j]);
+        sum+=arr[i][j];
         j++;
     }
     printf("tong cac ptu trong mang %d",sum);
